217f7e326711e26966563aa4a6a386a9f1a42527
Skuld <nike073i@mail.ru>
2021-10-13 14:50:19 +0400

    lab_3 is ready for check 13.10.2021 14:50

---
+++ ComputationalMathematicsLabs/Lab_3/СonstantStepNewtonMethod.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+
+namespace ComputationalMathematicsLabs.Lab_3
+{
+    public class СonstantStepNewtonMethod
+    {
+        private readonly double _epsilon;
+        private readonly double _startX;
+        private readonly double _step;
+        private readonly int _length;
+        private int _iter;
+
+        public delegate double Function(double x);
+        private readonly Function myFuction;
+
+        public СonstantStepNewtonMethod(double epsilon, double startX, double step, Function function)
+        {
+            if (epsilon <= 0 || epsilon >= 1 || step <= 0) throw new ArgumentException("Неверные значения входных данных");
+            _epsilon = epsilon;
+            _startX = startX;
+            _step = step;
+            myFuction = function;  
+            string epsilonString = string.Format("{0:#,#.#############################}", epsilon);
+            _length = epsilonString.Split(',')[1].Length;
+        }
+
+        private double StepMethod(double x, double oldX)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + "}", _iter)).Append(" ; ");
+
+            var result = x;
+            stringBuilder.Append(result.ToString("F" + (_length + 1)) + " ; ");
+
+            double fxn = myFuction(x);
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + ":F" + (_length + 1) + "}", fxn)).Append(" ; ");
+
+            double denominator = fxn * _step;
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + ":F" + (_length + 1) + "}", denominator)).Append(" ; ");
+
+            double xn_h = x + _step;
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + ":F" + (_length + 1) + "}", xn_h)).Append(" ; ");
+
+            double fxn_h = myFuction(xn_h);
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + ":F" + (_length + 1) + "}", fxn_h)).Append(" ; ");
+
+            double divider = fxn_h - fxn;
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + ":F" + (_length + 1) + "}", divider)).Append(" ; ");
+
+            double newX = x - (denominator / divider);
+            string delta = x == _startX ? "" : Math.Abs(oldX - x).ToString("F" + (_length + 1));
+            stringBuilder.Append(string.Format(@"{0," + (_length + 4) + ":F" + (_length + 1) + "}", delta)).Append(" ; ");
+
+            Console.WriteLine(stringBuilder.ToString());
+            if (Math.Abs(oldX - x) > _epsilon)
+            {
+                _iter++;
+                result = StepMethod(newX, x);
+            }
+
+            return result;
+        }
+
+        public void StartComputational()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format(@"{0," + (_length + 4) + "}", "n"));
+            sb.Append(string.Format(@"{0," + (_length + 6) + "}", "xn"));
+            sb.Append(string.Format(@"{0," + (_length + 7) + "}", "f(xn)"));
+            sb.Append(string.Format(@"{0," + (_length + 7) + "}", "f(xn)*h"));
+            sb.Append(string.Format(@"{0," + (_length + 7) + "}", "xn+h"));
+            sb.Append(string.Format(@"{0," + (_length + 7) + "}", "f(xn+h)"));
+            sb.Append(string.Format(@"{0," + (_length + 9) + "}", "f(xn+h)-f(xn)"));
+            sb.Append(string.Format(@"{0," + (_length + 6) + "}", "Delta"));
+            Console.WriteLine(sb.ToString());
+            _iter = 0;
+            StepMethod(_startX, 0);
+        }
+    }
+}
--- ComputationalMathematicsLabs/Program.cs
+++ ComputationalMathematicsLabs/Program.cs
@@ -1,4 +1,4 @@
-﻿using ComputationalMathematicsLabs.Lab_2;
+﻿using ComputationalMathematicsLabs.Lab_3;
 using System;
 
 namespace ComputationalMathematicsLabs
@@ -7,19 +7,18 @@ namespace ComputationalMathematicsLabs
     {
         public static void Main()
         {
-            double[,] testMatrixOne =
-            {
-                {2.923, 0.220, 0.159, 0.328},
-                {0.363, 4.123, 0.268, 0.327},
-                {0.169, 0.271, 3.906, 0.295},
-                {0.241, 0.319, 0.257, 3.862}
-            };
+            
             Console.WriteLine("Введите эпсилон (например: 0,00001)");
             double.TryParse(Console.ReadLine(), out double epsilon);
-            epsilon = epsilon == 0 ? 0.001 : epsilon;
-            double[] freeTermsVector = { 0.605, 0.496, 0.590, 0.896 };
-            var computational = new JacobiMethod(testMatrixOne, freeTermsVector, epsilon);
-            computational.StartComputational();
+            epsilon = epsilon == 0 ? 0.000001 : epsilon;
+            Console.WriteLine("Введите шаг (например: 0,001)");
+            double.TryParse(Console.ReadLine(), out double step);
+            step = step == 0 ? 0.001 : step;
+            Console.WriteLine("Введите начальное приближение (например: 2,0)");
+            double.TryParse(Console.ReadLine(), out double startX);
+            startX = startX == 0 ? 2 : startX;
+            СonstantStepNewtonMethod csnm = new СonstantStepNewtonMethod(epsilon, startX, step, (x) => Math.Exp(-0.5 * x) - 0.2 * x * x + 1);
+            csnm.StartComputational();
             Console.ReadKey();
         }
     }
